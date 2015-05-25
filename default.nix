0c28e7741f0d82bb5a479b42cfea5ed7f74efc8c
Paulus Esterhazy <pesterhazy@gmail.com>
2015-05-25 17:27:57 +0200

    WIP

---
+++ default.nix
@@ -0,0 +1,28 @@
+with import <nixpkgs> {};
+    with pkgs.python27Packages;
+    
+    buildPythonPackage { 
+      name = "impurePythonEnv";
+      buildInputs = [
+         git
+         libxml2
+         libxslt
+         libzip
+         python27Full
+         python27Packages.virtualenv
+         stdenv
+         zlib ];
+      src = null;
+      # When used as `nix-shell --pure`
+      shellHook = ''
+      unset http_proxy
+      export GIT_SSL_CAINFO=/etc/ssl/certs/ca-bundle.crt
+      source env/bin/activate
+      true
+      '';
+      # used when building environments
+      extraCmds = ''
+      unset http_proxy # otherwise downloads will fail ("nodtd.invalid")
+      export GIT_SSL_CAINFO=/etc/ssl/certs/ca-bundle.crt
+      '';
+    }
